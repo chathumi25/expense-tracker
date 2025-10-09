9396046fe972a4c4bdf1755b674faba623f5c2d3
Chathumi Rathnayaka <chathumi.rathnayaka25@gmail.com>
2025-10-09 16:02:24 +0530

    feat: add expense management and dashboard data functionality
    - Created new controllers: dashboardController.js and expenseController.js
    - Implemented add, get, delete, and download expense features
    - Updated Expense model with necessary fields
    - Modified expenseRoutes and dashboardRoutes for new endpoints
    - Integrated dashboard data retrieval into server.js

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const { 
-    addIncome,
-    getAllIncome, 
-    deleteIncome,
-    downloadIncomeExcel
-} = require("../controllers/incomeController");
+    addExpense,
+    getAllExpense, 
+    deleteExpense,
+    downloadExpenseExcel
+} = require("../controllers/expenseController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
-router.post("/add", protect, addIncome);
-router.get("/get", protect, getAllIncome);
-router.get("/downloadexcel", protect, downloadIncomeExcel);
-router.delete("/:id", protect, deleteIncome);
+router.post("/add", protect, addExpense);
+router.get("/get", protect, getAllExpense);
+router.get("/downloadexcel", protect, downloadExpenseExcel);
+router.delete("/:id", protect, deleteExpense);
 
 
 
